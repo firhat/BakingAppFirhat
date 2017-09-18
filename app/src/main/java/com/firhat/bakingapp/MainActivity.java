08485f8cbdacfebb7a4351b15c6e21e0e5bba4fc
FIrhat <firhathidayat@gmail.com>
2017-09-18 19:05:09 +0700

    - add testing - fix ui

--- app/src/main/java/com/firhat/bakingapp/MainActivity.java
+++ app/src/main/java/com/firhat/bakingapp/MainActivity.java
@@ -1,11 +1,32 @@
 package com.firhat.bakingapp;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.VisibleForTesting;
+import android.support.test.espresso.IdlingResource;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firhat.bakingapp.IdlingResource.SimpleIdlingResource;
 import com.firhat.bakingapp.fragments.RecipeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Nullable
+    private SimpleIdlingResource mIdlingResource;
+
+    /**
+     * Only called from test, creates and returns a new {@link SimpleIdlingResource}.
+     */
+    @VisibleForTesting
+    @NonNull
+    public IdlingResource getIdlingResource() {
+        if (mIdlingResource == null) {
+            mIdlingResource = new SimpleIdlingResource();
+        }
+        return mIdlingResource;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
             args.putBoolean(getString(R.string.is_tablet), true);
             recipeFragment.setArguments(args);
 
-            getSupportFragmentManager().beginTransaction().add(R.id.container_recipe, recipeFragment)
+            getSupportFragmentManager().beginTransaction().add(R.id.container_recipe_land, recipeFragment)
                     .commit();
         }
 
